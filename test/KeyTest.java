aa68c7b321c1a2cdf4560e635139be9b82d1cc05
Johannes Thyroff <44289588+JThyroff@users.noreply.github.com>
2020-10-30 19:22:14 +0100

    Add TestImage

--- test/KeyTest.java
+++ test/KeyTest.java
@@ -8,54 +8,62 @@ import java.io.*;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class KeyTest {
-
-    @Test
-    public void keyTest1() {
+    public static void main(String[] args) {
         ArrayList<MyPosition> expected = new ArrayList<>();
         expected.add(new MyPosition((short) 1, (short) 2, Channel.RED, (short) 4));
 
-        File original = new File("C:/Users/Roberto/Pictures/dualneg1.png");
+        File original = new File("test/dualneg1.png");
 
         copyFile(original);
-        File testFile = new File("C:/Users/Roberto/Pictures/dualneg1.pngcopy.png");
+        File testFile = new File("test/dualneg1.png" + "copy.png");
 
         try {
             Hider.writeKeyIntoImage(testFile, expected);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            ArrayList<MyPosition> myPositions = Revealer.extractKeyFromImage(testFile);
-            assertEquals(expected, myPositions);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    @Test
-    public void keyTestBlack() {
+    private void keyTestImg(String path) {
         ArrayList<MyPosition> expected = new ArrayList<>();
         expected.add(new MyPosition((short) 1, (short) 2, Channel.RED, (short) 4));
 
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        File original = new File("test/blankKey84.png");
+        File original = new File(path);
 
         copyFile(original);
-        File testFile = new File("test/blankKey84.pngcopy.png");
+        File testFile = new File(path + "copy.png");
 
         try {
             Hider.writeKeyIntoImage(testFile, expected);
         } catch (IOException e) {
             e.printStackTrace();
+            fail();
         }
         try {
             ArrayList<MyPosition> myPositions = Revealer.extractKeyFromImage(testFile);
             assertEquals(expected, myPositions);
         } catch (IOException e) {
             e.printStackTrace();
+            fail();
         }
+    }
+
+    @Test
+    public void keyTest1() {
+        keyTestImg("test/dualneg1.png");
+    }
+
+    @Test
+    public void keyTestBlack() {
+        keyTestImg("test/blankKey84.png");
+    }
+
+    @Test
+    public void keyTestWhite() {
+        keyTestImg("test/blankKey84White.png");
     }
 
     public static void copyFile(File original) {
